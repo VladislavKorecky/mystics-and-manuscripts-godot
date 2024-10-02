9c47670ab8456af379b5ec658df454ae8cace8dd
Vladislav Korecký <vladislav.ml@korecky.org>
2024-10-02 14:33:23 +0200

    Adding an Option component

---
+++ components/option/option.gd
@@ -0,0 +1,29 @@
+@tool
+extends PanelContainer
+
+
+# A unique string for referrencing this option. (displayed on left side)
+@export var identifier: String
+# Text describing the option.
+@export var content: String
+# Adds a background highlight when true.
+@export var highlighted: bool
+
+
+func _ready() -> void:
+	update()
+
+
+func _process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		update()
+
+
+func update() -> void:
+	"""
+	Update the option's appearance.
+	"""
+	
+	(%Label as Label).text = "%s. %s" % [identifier, content]
+	(%Label as Label).set("theme_override_colors/font_color", Color.BLACK if highlighted else Color.WHITE)
+	%Background.visible = highlighted
